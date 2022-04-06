3e6fb9257dbd7bac0c45e5b1276517fb58a92314
Asaia <14948113+asaiapalacios@users.noreply.github.com>
2022-04-05 20:39:13 -0500

    Use handler, props, state, & callback handler; Move list item to new component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
-// Invoke instances (2) of <AddTodoForm> and <TodoList> components in root <App> component
-let App = () => {
+function App() {
+  // Create a new state variable, newTodo, with its update function, setNewTodo
+  // Tell React that newTodo is a state that changes over time; whenever it changes, React has to re-render its affected component(s)
+  const [newTodo, setNewTodo] = React.useState("");
+
+  // Invoke instances (2) of <AddTodoForm> and <TodoList> components
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm />
+
+      <AddTodoForm onAddTodo={setNewTodo} />
+      {/* Render newTodo value from AddTodoForm component */}
+      <p>{newTodo}</p>
+
       <TodoList />
     </div>
   );
-};
+}
 
 export default App;
