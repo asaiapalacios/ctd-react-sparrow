f01cc9bb39ce8803004ca7aea1208a2e03ba68cf
Asaia <14948113+asaiapalacios@users.noreply.github.com>
2022-04-26 18:51:01 -0600

    Create custom hook with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
+// 2) Extract functionality as a reusable custom hook: synchronize local storage to React's state
+const useSemiPersistentState = () => {
+  // Make todo item(s) still visible after refreshing page (its default initial state) via...
+  // getItem which *reads* an object from local storage
+  // Note: use JSON API to transform (JSON.parse()) item(s) back from string to JavaScript object
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem("savedTodoList"))
+  );
+  // *Write*/save to browser local storage the state value in str when todoList state changes
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+};
+
 function App() {
-  const [todoList, setTodoList] = React.useState([]);
-  // B) Callback's parameter newTodo receives passed obj -> current state of user input + unique no.
+  // 1) Call custom hook to return locally stored state and updater function
+  const [todoList, setTodoList] = useSemiPersistentState();
+
+  // 4) Callback's parameter newTodo receives passed obj -> current state of user input + unique no.
   let addTodo = (newTodo) => {
     console.log(newTodo);
 
@@ -16,21 +34,22 @@ function App() {
     // Note: component currently displays PREVIOUS state
     // console.log(todoList);
   };
-  // Note: component re-renders to CURRENT state
+  // Note: component re-renders to CURRENT state of array of objects
   console.log(todoList);
 
   return (
-    <div>
+    // Insert fragment use when you don't want to introduce an element (div) to satisfy React rules
+    <>
       <h1>Todo List</h1>
-      {/* A) AddTodoForm instance:
+      {/* 3) AddTodoForm instance:
       -receives a passed object (current state of user input + unique no.) after form is submitted
-      ->reference to the handler function addTodo is now invoked, passing object as an argument */}
+      -> reference to the handler function addTodo is now invoked, passing object as an argument */}
       <AddTodoForm onAddTodo={addTodo} />
-      {/* C) After passing todoList state in its updated CURRENT form to <TodoList> component...
+      {/* 5) After passing todoList state (array of objects) in its updated CURRENT form to <TodoList> comp...
       -TodoList instance receives back what user has typed & submitted in a a <li> format, 
-      ->now ready to be rendered to React.DOM */}
+      -> now ready to be rendered into React.DOM */}
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
 
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// Return input field value (what user typed and submitted) in a <li> display
+// Return title/input field value (what user typed and submitted) in <li>
 function TodoListItem({ todo }) {
   return <li>{todo.title}</li>;
 }
